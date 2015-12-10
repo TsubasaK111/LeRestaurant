ea54b89f8801bd4c53554f7804bdedb2e4427d1d
TsubasaK111 <Tsubasa.K111@gmail.com>
2015-12-10 21:32:36 +0900

    refactor: remove unnecessary dependency module and commented out code

--- queries.py
+++ queries.py
@@ -8,12 +8,13 @@ Base.metadata.bind = engine
 DatabaseSession = sessionmaker(bind = engine)
 session = DatabaseSession()
 
-# firstPuppy = session.query(Puppy).first()
-# print firstPuppy.name
-
 dishes = session.query(MenuItem).all()
 for dish in dishes:
     print 'working,...'
     print dish.name
     print "\n"
     print "\n"
+
+restaurants = session.query(Restaurant).all()
+for restaurant in restaurants:
+    print restaurant.name
