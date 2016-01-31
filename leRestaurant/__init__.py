ef1ab1706bd559fa2bf830040d46a7f0522e649c
TsubasaK111 <Tsubasa.K111@gmail.com>
2016-01-31 23:17:49 +0900

    refactor: pull config settings from PUPPIES SETTINGS bash envvar

--- leRestaurant/__init__.py
+++ leRestaurant/__init__.py
@@ -8,13 +8,13 @@ app = Flask(__name__)
 
 
 app.config.from_pyfile('default_config.py')
-app.config.from_envvar('PUPPIES_SETTINGS', silent=True)
+app.config.from_envvar('LERESTAURANT_SETTINGS', silent=True)
 
 logging.basicConfig( filename='log.log',
                      level=logging.DEBUG,
                      format='%(levelname)s: %(message)s [%(asctime)s]',
                      datefmt='%m/%d/%Y %I:%M:%S %p' )
-                     
+
 # create console handler, set level to debug and add to logger
 consoleHandler = logging.StreamHandler()
 consoleHandler.setLevel(logging.DEBUG)
